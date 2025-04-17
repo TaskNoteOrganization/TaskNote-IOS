859ba3095d0b3f48b02e8b79dc15197be1161b72
Edward Leon <edleon761@gmail.com>
2025-04-17 14:34:08 -0700

    Created a views folder

---
+++ TaskNoteIOS/Views/HomeNavBar.swift
@@ -0,0 +1,21 @@
+//
+//  HomeNavBar.swift
+//  TaskNoteIOS
+//
+//  Created by Edward Leon on 4/17/25.
+//
+
+import SwiftUI
+
+struct HomeNavBar: View {
+    var body: some View {
+        ZStack
+        {
+            Rectangle()
+        }
+    }
+}
+
+#Preview {
+    HomeNavBar()
+}
---
+++ TaskNoteIOS/Views/NoteListView.swift
@@ -0,0 +1,24 @@
+//
+//  NoteListView.swift
+//  TaskNoteIOS
+//
+//  Created by Edward Leon on 4/17/25.
+//
+
+import SwiftUI
+
+struct NoteListView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    NoteListView()
+}
