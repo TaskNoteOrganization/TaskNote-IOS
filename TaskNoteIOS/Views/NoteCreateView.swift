2f6a3bcba0f0106d1ae674d26fd7aaf1010ccf36
Adrian Haro <aharo1110@gmail.com>
2025-04-17 16:01:49 -0700

    Create simple editor with Markdown

--- TaskNoteIOS/Views/NoteCreateView.swift
+++ TaskNoteIOS/Views/NoteCreateView.swift
@@ -6,10 +6,56 @@
 //
 
 import SwiftUI
+import Supabase
+import MarkdownUI
 
 struct NoteCreateView: View {
+    
+    @State private var noteTitle: String = ""
+    @State private var noteText: String = ""
+    @State private var isPreview: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Note Title", text: $noteTitle).font(.system(size: 26)).fontWeight(.semibold)
+            ZStack {
+                // The editing field
+                VStack {
+                    TextEditor(text: $noteText)
+                        .frame(height: 500.0, alignment: .top).monospaced()
+                        .keyboardType(.asciiCapable).font(.system(size: 15))
+                        .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(Color.gray, lineWidth: 2)
+                            )
+                }.opacity(isPreview ? 0 : 1)
+                // The Markdown preview
+                VStack {
+                    Markdown(noteText).frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                    
+                }.opacity(isPreview ? 1 : 0)
+                    .frame(height: 500.0)
+            }
+            HStack {
+                Button(action: preview){
+                    Text(isPreview ? "Continue Editing" : "Preview")
+                }
+                Spacer()
+                Button(action: submit){
+                    Text("Submit")
+                }
+            }
+        }
+        .padding()
+    }
+    
+    func preview(){
+        isPreview.toggle()
+    }
+    
+    func submit(){
+        // TODO
+        return
     }
 }
 
